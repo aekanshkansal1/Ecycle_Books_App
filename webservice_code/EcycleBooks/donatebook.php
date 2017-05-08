06cfd10f8fd0d61868be2580c23c6065ce24545d
aekanshkansal1 <aekanshkansal1@gmail.com>
2017-05-09 01:58:37 +0530

    webservice change for app updates

--- webservice_code/EcycleBooks/donatebook.php
+++ webservice_code/EcycleBooks/donatebook.php
@@ -6,34 +6,28 @@ $df=new db_function();
 $response=Array("error"=>false);
 
 //If school is not there it will get NA
-if(isset($_POST['userid']) && isset($_POST['bookid']) && isset($_POST['edition'])) {
+if(isset($_POST['userid']) && isset($_POST['edition']) && isset($_POST['bookname']) && isset($_POST['author']) && 
+    isset($_POST['publication'])) {
     $userid = $_POST['userid'];
-    $bookid = $_POST['bookid'];
+    $bookname=$_POST['bookname'];
+    $author=$_POST['author'];
+    $publication=$_POST['publication'];
     $edition = $_POST['edition'];
-
-    if (($duplicate = $df->duplicatePosts($userid, $bookid, $edition,'donated')) == true) {
-        $response["error"] = true;
-        $response["status"] = "failure";
-        $response["error_msg"] = "Already Posted Book For Sale.";
-        echo json_encode($response);
-    }
-    else if ($duplicate == -1) {
-        $response["error"] = true;
-        $response["status"] = "failure";
-        $response["error_msg"] = "Error in Posting Data.";
-        echo json_encode($response);
-    }
-    else {
-        if ($df->donateBooks($userid, $bookid, $edition)) {
+    if($publication=="")
+        $publication="NA";
+    
+    if ($df->donateBooks($userid,$bookname,$author,$publication,$edition))
+    {
             $response["error"] = false;
             $response["status"] = "success";
             echo json_encode($response);
-        }
-        else {
+    }
+    else 
+    {
             $response["error"] = true;
             $response["status"] = "failure";
             $response["error_msg"] = "Error in Posting Data.";
-            echo json_encode($response);        }
+            echo json_encode($response);
     }
 }
 else
--- webservice_code/EcycleBooks/sellbook.php
+++ webservice_code/EcycleBooks/sellbook.php
@@ -6,27 +6,20 @@ $df=new db_function();
 $response=Array("error"=>false);
 
 //If school is not there it will get NA
-if(isset($_POST['userid']) && isset($_POST['bookid']) && isset($_POST['mrp'])&& isset($_POST['price']) && isset($_POST['edition'])) {
+if(isset($_POST['userid']) && isset($_POST['bookname']) && isset($_POST['author']) && isset($_POST['publication']) && 
+    isset($_POST['mrp']) && isset($_POST['price']) && isset($_POST['edition'])) {
     $userid = $_POST['userid'];
-    $bookid = $_POST['bookid'];
+    $bookname = $_POST['bookname'];
+    $author=$_POST['author'];
+    $publication=$_POST['publication'];
     $mrp = $_POST['mrp'];
     $price = $_POST['price'];
     $edition = $_POST['edition'];
 
-    if (($duplicate = $df->duplicatePosts($userid, $bookid,$edition,'used')) == true) {
-        $response["error"] = true;
-        $response["status"] = "failure";
-        $response["error_msg"] = "Already Posted Book For Sale.";
-        echo json_encode($response);
-    }
-    else if ($duplicate == -1) {
-        $response["error"] = true;
-        $response["status"] = "failure";
-        $response["error_msg"] = "Error in Posting Data.";
-        echo json_encode($response);
-    }
-    else {
-        if ($df->sellBooks($userid, $bookid, $mrp, $price, $edition)) {
+        if($publication=="")
+            $publication="NA";
+
+        if ($df->sellBooks($userid, $bookname,$author,$publication,$mrp,$price,$edition)) {
             $response["error"] = false;
             $response["status"] = "success";
             echo json_encode($response);
@@ -35,9 +28,9 @@ if(isset($_POST['userid']) && isset($_POST['bookid']) && isset($_POST['mrp'])&& 
             $response["error"] = true;
             $response["status"] = "failure";
             $response["error_msg"] = "Error in Posting Data.";
-            echo json_encode($response);        }
+            echo json_encode($response); 
+        }
     }
-}
 else
 {
     $response["error"]=true;
